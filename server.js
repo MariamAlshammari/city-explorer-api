217eb62eff0be4124aaf549d003a5eadc44db254
mariamalshammari <malshammari37@gmail.com>
2021-07-06 23:22:06 +0300

    firstBackEnd

--- server.js
+++ server.js
@@ -30,13 +30,33 @@ server.get('/test',(request,response)=>{
  
 // http://localhost:3001/weather?locationQuery=Seattle&&lon=-122.33207&&lat=47.60621
 server.get('/weather',(req,res)=>{
-    console.log(req.query);
+    // console.log(req.query);
+    try{
     let selectedCity = weatherData.find (city =>{
-        if(city.city_name== req.query.locationQuery && city.lon===req.query.lon && city.lat== req.query.lat) {
+        if(city.city_name== req.query.locationQuery && city.lon==req.query.lon && city.lat== req.query.lat) {
             return city
         }
-    })
-    res.status(200).send(selectedCity);
+        
+    }) 
+        // res.status(200).send(selectedCity); 
+    //    { console.log(selectedCity);}
+     const cityWeatherProp=selectedCity.data.map(item=>{
+         // console.log(cityWeatherProp);
+         return new ForeCast(item.valid_date,item.weather.description);
+ 
+     })
+    //  console.log(cityWeatherProp);
+
+     res.status(200).send(cityWeatherProp); }
+     catch(err){
+        res.status(500)
+        res.send(err.message)
+
+        
+        }
+ 
+        
+    
 })
 
 server.get('*',(req,res)=>{
@@ -45,6 +65,13 @@ server.get('*',(req,res)=>{
 
 
 
+class ForeCast {
+    constructor(date,description){
+     this.date=date;
+     this.description=description;
+     // this.city_name=weathInfo.city_name
+   }
+ };
 
 
 
